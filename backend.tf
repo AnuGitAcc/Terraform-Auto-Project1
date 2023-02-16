5c49d1c357266dcec9ba7603b97f2dc57d93acee
anushri <anushri@gmail.com>
2023-02-16 19:18:13 +0530

    This commit is for change in backend file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket-TFP2023"
+    bucket = "my-bucket-TFP2023"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
